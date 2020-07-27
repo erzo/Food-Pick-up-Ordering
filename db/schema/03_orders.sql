53e44c6f894ca628ce90e0ae25c1ba044514a21f
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-27 18:05:33 +0000

    created entity for orders

---
+++ db/schema/03_orders.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  customer_notes text,
+  initial_order_time TIME,
+  estimated_wait_time TIME,
+  order_status VARCHAR(255),
+  pickup_time TIME
+);
